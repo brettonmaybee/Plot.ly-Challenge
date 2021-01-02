d2b96e09f7f364a6004585d2ef5eff583ddec683
brettonmaybee <brettonmaybee@users.noreply.github.com>
2021-01-02 06:55:15 -0800

    Step 1: Plotly
    D3 library used to read in 'samples.json'

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const datapath = "./data/samples.json";
+
+// Fetch the JSON data and console log it
+d3.json(datapath).then(function(data) {
+  console.log(data);
+});
+
+// Promise Pending
+const dataPromise = d3.json(datapath);
+console.log("Data Promise: ", dataPromise);
